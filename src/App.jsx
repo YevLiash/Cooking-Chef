6f3c9fad6f68104b345bdca55021d83f844eef12
Yevhenii Liashenko <opanabl9@gmail.com>
2026-01-01 15:10:01 +0100

    initial app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      App component
+    </div>
+  )
+}
+
+export default App
